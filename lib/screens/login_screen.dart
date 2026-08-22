e06257da9b866a12bd9bc7bce4892e298934305a
Maxr1998 <max.rumpf1998@gmail.com>
2026-08-22 18:12:25 +0200

    Fix LoginAuxilliaryOptions getting covered by keyboard

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,10 @@
 import 'package:finamp/components/Buttons/simple_button.dart';
 import 'package:finamp/components/LoginScreen/login_flow.dart';
+import 'package:finamp/l10n/app_localizations.dart';
 import 'package:finamp/screens/advanced_login_options_screen.dart';
 import 'package:finamp/screens/language_selection_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
-import 'package:finamp/l10n/app_localizations.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -36,7 +36,7 @@ class _LoginAuxillaryOptions extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 12.0),
+        padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: MediaQuery.viewInsetsOf(context).bottom + 12.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
